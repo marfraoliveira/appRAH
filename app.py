dcb9fb18c659f8e050f9aef03dfff428a40b736a
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-03-25 16:49:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 from flask import Flask,request,jsonify
 import numpy as np
 import pickle
+import json
 
 model = pickle.load(open('model.pkl','rb'))
 
@@ -11,43 +11,21 @@ app = Flask(__name__)
 def index():
     return "Hello world"
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
-
-    input_query = np.array([[cgpa,iq,profile_score]])
-
-    result = model.predict(input_query)[0]
-
-    return jsonify({'placement':str(result)})
-
-if __name__ == '__main__':
-=======
-from flask import Flask,request,jsonify
-import numpy as np
-import pickle
-
-
-model = pickle.load(open('modelkNN100Hz.pkl','rb'))
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Eficiência de Energia em dispositivos Móveis"
-
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    x_axis = float((request.form.get('x_axis')))
-    y_axis = float((request.form.get('y_axis')))
-    z_axis = float((request.form.get('z_axis')))
-    input_query = np.array([[x_axis,y_axis,z_axis]])
+    cgpa = float(request.form.get('cgpa'))
+    iq = float(request.form.get('iq'))
+    profile_score = float(request.form.get('profile_score'))
+    #input_query = np.array([[float(cgpa),float(iq),float(profile_score)]])
+    input_query = np.array([[(cgpa),(iq),(profile_score)]])
+    teste = dict({"cgpa":0.1,"iq":12,"profile_score":90})
     result = model.predict(input_query)[0]
-    return jsonify({'Movimento':str(result)})
+    return jsonify({'Movimento':str(result),
+                    'teste':teste
+                    })
+
+
 
 if __name__ == '__main__':
->>>>>>> 7c5524e (primeiro commit)
     app.run(debug=True)
