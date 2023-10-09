306922bd99fe4019ba7eb9d257d2f5922d62a147
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-09 13:21:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,11 @@ from json import JSONEncoder
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from flask_cors import CORS
 
 app = Flask(__name__)
-#%%
+CORS(app)  # Isso permite solicitações de qualquer origem
 
-
-#%%
 # Carregue o modelo uma vez ao iniciar o servidor Flask
 
 @app.route('/', methods=['GET'])
