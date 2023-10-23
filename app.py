878a35a240772efc54703e7a8dca1b8d3f542a78
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-22 21:04:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,56 +8,34 @@ import pandas as pd
 import keras.models
 from keras.models import model_from_json
 import json
-import time
 from json import JSONEncoder
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
+#%%
+
 
 #%%
-# Carregue o modelo uma vez ao iniciar o servidor Flask
-model = load_model('./modelCNN.h5')
-# Defina a opção para executar em modo eager
-tf.config.run_functions_eagerly(True)
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
-
 @app.route('/api', methods=['POST'])
-def predict():
+def receber_dados():
     try:
-        # Obtém o JSON da solicitação
-        data = request.get_json()
-        # Pré-processamento dos dados
-        columns = ['x', 'y', 'z']
-        df = pd.DataFrame(data, columns=columns)
-        df['x'] = df['x'].astype('float')
-        df['y'] = df['y'].astype('float')
-        df['z'] = df['z'].astype('float')
-        data = df.to_numpy()
-        data = data.reshape(-1, 90, 3)
-        
-        # Faça uma única previsão com o modelo carregado
-        class_predict = np.argmax(model.predict(data), axis=1)
-        
-        # Mapear as previsões para as categorias
-        category_mapping = {
-            0: 'Walking',
-            1: 'Jogging',
-            2: 'Upstairs',
-            3: 'Downstairs',
-            4: 'Sitting',
-            5: 'Standing'
-        }
-        
-        class_labels = [category_mapping[pred] for pred in class_predict]
-        
-        return jsonify({'predicted_categories': class_labels})
+        dados = request.get_json()  # Obter dados JSON da requisição
+
+        # Contar o número de registros recebidos
+        numero_de_registros = len(dados)
+
+        print("Número de registros recebidos:", numero_de_registros)
+        print("Dados recebidos:", dados)
+
+        return jsonify({"status": "Dados recebidos com sucesso"})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({"status": "Erro ao processar os dados", "erro": str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
+
+
+
