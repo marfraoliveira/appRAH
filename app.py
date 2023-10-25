2859f8d61acaa537b8e4f9c288deb936bb33b45b
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-25 06:34:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
-import time
-from distutils.log import debug
-from fileinput import filename
-from flask import *  
-from flask import Flask,request,jsonify  
-import numpy as np
-import pandas as pd
-import keras.models
-from keras.models import model_from_json
-import json
-from json import JSONEncoder
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-import jsonschema
-from jsonschema import validate
-
-app = Flask(__name__)
-#%%
-# Defina o esquema JSON
 schema = {
     "type": "object",
     "properties": {
-        "x": {"type": "number"},
-        "y": {"type": "number"},
-        "z": {"type": "number"},
-        "timestamp": {"type": "string", "format": "date-time"}
+        "x": { "type": "number" },
+        "y": { "type": "number" },
+        "z": { "type": "number" },
+        "timestamp": { "type": "string" }
     },
     "required": ["x", "y", "z", "timestamp"]
 }
+
+
 #%%
 @app.route('/api', methods=['POST'])
 def receber_dados():
@@ -41,7 +23,3 @@ def receber_dados():
     except Exception as e:
        # Erro de validação
        return jsonify({"error": str(e)}), 400
-
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
