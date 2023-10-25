5b59ee25b45aa671cc5de6addb3fee8221c8122f
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-25 09:11:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-schema = {
-    "type": "object",
-    "properties": {
-        "x": { "type": "number" },
-        "y": { "type": "number" },
-        "z": { "type": "number" },
-        "timestamp": { "type": "string" }
-    },
-    "required": ["x", "y", "z", "timestamp"]
-}
+import time
+from distutils.log import debug
+from fileinput import filename
+from flask import *  
+from flask import Flask,request,jsonify  
+import numpy as np
+import pandas as pd
+import keras.models
+from keras.models import model_from_json
+import json
+from json import JSONEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+#%%
 
 
 #%%
 @app.route('/api', methods=['POST'])
 def receber_dados():
-    # Suponha que você tenha recebido dados JSON
-    dados_json = request.get_json()
     try:
-       # Tente validar os dados recebidos com o esquema
-       validate(instance=dados_json, schema=schema)
-       # Os dados são válidos
-       return jsonify({"status": "Dados válidos"}), 200
+        dados = request.get_json()  # Obter dados JSON da requisição
+        
+        
+        print("Número de registros recebidos:", numero_de_registros)
+        print("Dados recebidos:", dados)
+
+        return jsonify({"status": str(dados)})
     except Exception as e:
-       # Erro de validação
-       return jsonify({"error": str(e)}), 400
+        return jsonify({"status": "Erro ao processar os dados", "erro": str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
