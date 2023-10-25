e096382aa319a8a6ff6e1f8f8c28361177ecb2a3
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-25 13:24:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def receber_dados():
 # =============================================================================
     try:
         dados = request.get_json()  # Obter dados JSON da requisição
-        return jsonify({"status": str(dados)})
+        return jsonify({"status dos dados recebidos": str(dados)})
     except Exception as e:
         return jsonify({"status": "Erro ao processar os dados", "erro": str(e)})
 
