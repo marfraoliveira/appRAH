fabd06461867c047058196556c9b6103ef99eee8
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-10-30 21:59:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,19 @@ def receber_dados():
 #     except Exception as e:
 #         return jsonify({"status": "Erro ao processar os dados", "erro": str(e)})
 # =============================================================================
-   try:
-        dados = [
-            {"data": [{"x": -1.8440361022949219, "y": 6.280742645263672, "z": 7.210247039794922, "timestamp": "2023-10-25 09:37:40.917324"}, {"x": -1.8440361022949219, "y": 6.280742645263672, "z": 7.210247039794922, "timestamp": "2023-10-25 09:37:40.917402"}, {"x": -1.5227222442626953, "y": 6.109155654907227, "z": 7.767829895019531, "timestamp": "2023-10-25 09:37:40.980064"}, {"x": -1.5227222442626953, "y": 6.109155654907227, "z": 7.767829895019531, "timestamp": "2023-10-25 09:37:40.980143"}, {"x": -1.5538654327392578, "y": 6.10975456237793, "z": 7.692966461181641, "timestamp": "2023-10-25 09:37:41.060983"}]}
-        ]
+    try:
+       dados = request.get_json()  # Obter dados JSON da requisição
 
+        # Contar o número de registros recebidos
+       #numero_de_registros = len(dados)
+
+       #print("Número de registros recebidos:", numero_de_registros)
+       #print("Dados recebidos:", dados)
         # Remover o último registro da lista 'dados'
-        if dados and 'data' in dados[0] and isinstance(dados[0]['data'], list) and len(dados[0]['data']) > 0:
-            dados[0]['data'].pop()
-
-        print(dados)
-        return jsonify({"status dos dados recebidos": str(dados)})
+       #if dados and 'data' in dados[0] and isinstance(dados[0]['data'], list) and len(dados[0]['data']) > 0:
+        #  dados[0]['data'].pop()
+       print(dados)
+       return jsonify({"status dos dados recebidos": str(dados)})
    except Exception as e:
         return jsonify({"status": "Erro ao processar os dados", "erro": str(e)})
 
