75b7c542b383ec9ec482d3f43e525511c52733f5
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-11-21 16:17:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from jsonschema import validate
 # CARREGAR O MODELO DE DL
 # =============================================================================
 # Model saved with Keras model.save()
-MODEL_PATH = 'modelCNN.h5'
+MODEL_PATH = 'model.h5'
 
 #Load your trained model
 model = load_model(MODEL_PATH)
