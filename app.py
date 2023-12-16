f4138e3e213fa014ff9d1d9c18d78ed522fc7f8f
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-12-16 15:13:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,130 +16,94 @@ import jsonschema
 from jsonschema import validate
 from sklearn.metrics import accuracy_score
 from threading import Timer
+from flask_socketio import SocketIO, emit
 
-# =============================================================================
 
-# =============================================================================
-# CARREGAR O MODELO DE DL
-# =============================================================================
-# Model saved with Keras model.save()
+app = Flask(__name__)
+
 MODEL_PATH = 'modelExit.h5'
-
-#Load your trained model
 model = load_model(MODEL_PATH)
 
 # Verifique se o modelo foi carregado com sucesso
 if isinstance(model, keras.models.Model):
-    print("O modelo" + MODEL_PATH + " foi carregado com sucesso.")
+    print(f"O modelo {MODEL_PATH} foi carregado com sucesso.")
 else:
     print("Ocorreu um erro ao carregar o modelo.")
 
-app = Flask(__name__)
-#%%
+async def fazer_previsoes_assincrono(model, janelas_deslizantes, n_janelas_por_predicao, category_mapping):
+    previsoes_totais = np.array([])
+
+    for i in range(0, len(janelas_deslizantes), n_janelas_por_predicao):
+        grupo_janelas = janelas_deslizantes[i:i + n_janelas_por_predicao]
+        previsao_grupo = model.predict(np.array(grupo_janelas))
+        previsoes_totais = np.append(previsoes_totais, previsao_grupo)
+
+    # Converta as previsões para classes previstas
+    previsoes_totais = previsoes_totais.reshape(-1, len(category_mapping))
+    classes_previstas = np.argmax(previsoes_totais, axis=1)
+    classificacoes = [category_mapping[class_index] for class_index in classes_previstas]
+
+    return classificacoes
 
 @app.route('/api', methods=['POST'])
-def receber_dados():
+async def receber_dados():
     try:
-        #data = 
+        
         data = request.get_json()
+
         if "data" in data:
-        # Excluindo o último registro se estiver mal formado
             if not all(key in data["data"][-1] for key in ["x", "y", "z", "timestamp"]):
                 del data["data"][-1]
-# Recompondo o JSON
+
         recomposed_json = json.dumps(data, indent=4)
-# Convertendo a string JSON para uma lista Python
         lista_python = json.loads(recomposed_json)['data']
-        #print(lista_python)    
-# =============================================================================
-# pré-processamento
-# =============================================================================
+
         parsed_data = json.loads(recomposed_json)
         data = np.array([[item["x"], item["y"], item["z"]] for item in parsed_data["data"]])
+
         columns = ['x', 'y', 'z']
-        df = pd.DataFrame(data,columns=columns)
+        df = pd.DataFrame(data, columns=columns)
         df['x'] = df['x'].astype('float')
         df['y'] = df['y'].astype('float')
         df['z'] = df['z'].astype('float')
-        #print(df)
         data = df.to_numpy()
-        #data = data[:len(data)//10] # Pego 10% dos dados enviados
-        tamanho_data = data.size
-        print('Quantidade de registros: '+str(len(lista_python)))
-        #print('tamanho dos dados numpy: '+str(tamanho_data))
-        #print('Dados Numpy:' + str(data) )
-# =============================================================================
-# Pre processamento novo
+
         if data.shape[1:] != (90, 3):
-            # Determine quantos registros devem ser descartados ou ajustados
             ajuste_necessario = data.shape[1] - 90
-            
-            # Descarte os primeiros registros
             data = data[:, ajuste_necessario:]
-# =============================================================================
-        
-          # Parâmetros da janela deslizante
-            tamanho_janela = 90  # Defina o tamanho da janela conforme necessário
 
-          # Crie todas as janelas deslizantes
-            janelas_deslizantes = []
-# =============================================================================
-            for i in range(len(data) - tamanho_janela + 1):
-              janela_deslizante = data[i:i + tamanho_janela]
-              janelas_deslizantes.append(janela_deslizante)
-# =============================================================================
-# Converta as janelas para um array numpy
-            janelas_deslizantes = np.array(janelas_deslizantes)
-    
-# Agora você pode usar 'janelas_deslizantes' conforme necessário em seu código
-          # por exemplo, imprimir uma janela:
-            print("Primeira janela deslizante:")
-            print(janelas_deslizantes[0])
-            
-# Inicialize um array para armazenar as previsões
-            previsoes = np.array([])
-                
-# Faça previsões para cada janela deslizante
-            category_mapping = {
-                  0: 'Walking',
-                  1: 'Jogging',
-                  2: 'Upstairs',
-                  3: 'Downstairs',
-                  4: 'Sitting',
-                  5: 'Standing'
-            }
-# Inicialize um array para armazenar as previsões
-            previsoes = np.array([])
-            classificacoes_list = []
+        tamanho_janela = 90
+        janelas_deslizantes = []
 
-# Ajuste o número de janelas a serem processadas em cada iteração
-            n_janelas_por_predicao = 500
-            
-# Faça previsões para cada grupo de n_janelas_por_predicao janelas deslizantes
-            #for i in range(0, len(janelas_deslizantes), n_janelas_por_predicao):
-                #grupo_janelas = janelas_deslizantes[i:i + n_janelas_por_predicao]
-                #previsao_grupo = model.predict(np.array(grupo_janelas))
-                #previsoes = np.append(previsoes, previsao_grupo)
-                
-# Converta as previsões para as classes previstas
-                #previsoes = previsoes.reshape(-1, len(category_mapping))
-                #classes_previstas = np.argmax(previsoes, axis=1)
-                #classificacoes = [category_mapping[class_index] for class_index in classes_previstas]
+        for i in range(len(data) - tamanho_janela + 1):
+            janela_deslizante = data[i:i + tamanho_janela]
+            janelas_deslizantes.append(janela_deslizante)
 
-            
-            
-                # Adicione as classificações à lista
-                #classificacoes_list.extend(classificacoes)
-    
-        try:
-            return jsonify({'Reconhecimento': str('Classificacao da atividade: '+ str( n_janelas_por_predicao)), 'O retorno eh bem formado': True})
-        except json.JSONDecodeError as json_error:
-            return jsonify({'error': f'JSON recomposto mal formado: {json_error}', 'is_well_formed': False})       
-        
+        janelas_deslizantes = np.array(janelas_deslizantes)
+
+        print("Primeira janela deslizante:")
+        print(janelas_deslizantes[0])
+
+        previsoes = np.array([])
+        category_mapping = {0: 'Walking', 1: 'Jogging', 2: 'Upstairs', 3: 'Downstairs', 4: 'Sitting', 5: 'Standing'}
+        previsoes = np.array([])
+        classificacoes_list = []
+        n_janelas_por_predicao = 1000
+
+        classificacoes_list = await fazer_previsoes_assincrono(model, janelas_deslizantes, n_janelas_por_predicao, category_mapping)
+
+        #return jsonify({'Reconhecimento': f'Classificacao da atividade: {classificacoes_list}', 'O retorno eh bem formado': True})
+        return jsonify(str(classificacoes_list))
+
+    except json.JSONDecodeError as json_error:
+        return jsonify({'error': f'JSON recomposto mal formado: {json_error}', 'is_well_formed': False})
     except Exception as e:
         return jsonify({'error': str(e)})
-   
- # =============================================================================
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    import asyncio
+    from aiohttp import web
+
+    loop = asyncio.get_event_loop()
+    app.config['loop'] = loop
+    app.run(debug=False, host='0.0.0.0', port=5001)
