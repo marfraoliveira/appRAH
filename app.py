4f5fa86a06382bd7a3314950dbc92fd4b8232101
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-12-21 22:38:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def receber_dados():
     #return jsonify({'Classificação:': atividade_predita_final})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run(debug=False, port=8000, host='0.0.0.0')
