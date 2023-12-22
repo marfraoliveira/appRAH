cd6fc805fbcce3bc4e54671b8874fda52712cd40
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-12-22 14:15:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def receber_dados():
     
 
         try:
-            return jsonify({'Reconhecimento': str('Classificacao da atividade: '+ str(previsoes)), 'O retorno eh bem formado': True})
+            return jsonify({'Reconhecimento': str('Classificacao da atividade: '+ str(category_mapping)), 'O retorno eh bem formado': True})
         except json.JSONDecodeError as json_error:
             return jsonify({'error': f'JSON recomposto mal formado: {json_error}', 'is_well_formed': False})       
         
