2de0a004379551f9320da71acee7f2c245c1fdd6
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-12-21 21:44:48 -0300

    Update load.py

--- load.py
+++ load.py
@@ -1,24 +1,20 @@
 import numpy as np
 import keras.models
 from keras.models import model_from_json
-from scipy.misc import imread, imresize,imshow
 import tensorflow as tf
 
 
 def init(): 
-	json_file = open('modeloCNN.json','r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model = model_from_json(loaded_model_json)
-	#load weights into new model
-	loaded_model.load_weights("modeloCNN.h5")
-	print("Loaded Model from disk")
+    json_file = open('modeloCNN.json','r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("modeloCNN.h5")
+    print("Modelo carregado com Sucesso!")
 
-	#compile and evaluate loaded model
-	loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-	#loss,accuracy = model.evaluate(X_test,y_test)
-	#print('loss:', loss)
-	#print('accuracy:', accuracy)
-	graph = tf.get_default_graph()
+    # compile and evaluate loaded model
+    loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-	return loaded_model,graph
+    return loaded_model
+
